780ab41172edd6a7200512ded6c62fd1908d364f
matheusrgg <matheusrgg@gmail.com>
2023-07-31 15:41:50 -0300

    filter exercise - first try

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,28 +10,42 @@ const Expenses = (props) => {
   const saveFilterDataHandler = (enteredFilterData) => {
     setFilteredYear(enteredFilterData)
     //estou escutando  a seleção do ano
-    console.log("informação do filtro", filteredYear)
+    console.log("informação do filtro 1", filteredYear)
+    console.log("informação do filtro 2", enteredFilterData)
     //fazer um filtro da array com esses
     //criar uma nova array talvez?
   }
 
   return (
     <div>
-     
+
       <Card className="expenses">
-      <FilterExpense
-        select={filteredYear}
-        onSaveFilterData={saveFilterDataHandler}
-      />
-      {props.items.map((expense)=> (
+        <FilterExpense
+          select={filteredYear}
+          onSaveFilterData={saveFilterDataHandler}
+        />
+
+
+        {/* {props.items.filter(expenses => expenses.date.getFullYear() == filteredYear).map(expense =>(
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
           />
-      ))}
-     
+      ))} */}
+
+        {console.log("o que vem aqui?", props.items[0].date.getFullYear())}
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+
+
       </Card>
     </div>
 
